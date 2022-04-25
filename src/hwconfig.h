e8568de5a37127b192b4950868eae88bd00415bd
Robert Ellis <rob@pigatron-industries.co.uk>
2022-04-25 08:37:31 +0100

    initial code commit

---
+++ src/hwconfig.h
@@ -0,0 +1,7 @@
+
+// Teensy Pins
+
+
+// Other config
+#define SERIAL_BAUD 115200
+#define SAMPLE_RATE 44000
---
+++ src/main.cpp
@@ -0,0 +1,23 @@
+#include <Arduino.h>
+#include <eurorack.h>
+#include <SPI.h>
+#include "hwconfig.h"
+//#include "MainController.h"
+
+//MainController mainController = MainController(SAMPLE_RATE);
+
+
+void setup() {
+    Serial.begin(SERIAL_BAUD);
+    Serial.println();
+    Serial.println("=========================================");
+    Serial.println("*     Pigatron Industries OctaSource    *");
+    Serial.println("=========================================");
+    Serial.println();
+
+    //mainController.init();
+}
+
+void loop() {
+    //mainController.update();
+}
