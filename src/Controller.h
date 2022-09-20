c06c81f6cb2bfdb8ad9214cc6483a6fb8199bf66
Robert Ellis <rob@pigatron-industries.co.uk>
2022-09-20 22:32:58 +0100

    move methods to abstract controller

--- src/Controller.h
+++ src/Controller.h
@@ -4,16 +4,13 @@
 #include <eurorack.h>
 #include "Hardware.h"
 
-class Controller {
+class Controller : public AbstractController {
     public:
         Controller(int lastMode = 0) : mode(0, lastMode) {}
         virtual void init(float sampleRate) { this->sampleRate = sampleRate; }
         virtual void init() {};
         virtual void update() = 0;
         virtual void process() = 0;
-        virtual int cycleMode(int amount) { mode.cycle(amount); return mode.value; }
-        virtual void cycleValue(int amount) {}
-        void setMode(int value) { mode.setValue(value); }
         float getSampleRate() { return sampleRate; }
     
     protected: 
