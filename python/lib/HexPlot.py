03a8622549a8c9f4533563e91c4d2b3c888ba357
Robert Ellis <rob@pigatron-industries.co.uk>
2024-07-08 21:34:38 +0100

    keyboard orientation

--- python/lib/HexPlot.py
+++ python/lib/HexPlot.py
@@ -1,9 +1,10 @@
 import matplotlib.pyplot as plt
-import matplotlib.patches as patches
+from matplotlib import patches, transforms
 import lib.sethares as sethares
 from lib.tuning import Tuning, Note
 from dataclasses import dataclass
 from typing import List
+import math
 
 
 @dataclass
@@ -33,18 +34,25 @@ class HexPlot():
         return None
 
 
-    def plotIsomorphic(self, start_note, x_increment, y_increment):
-        fig, ax = plt.subplots(figsize=(10, 12))
+    def plotIsomorphic(self, start_note, x_increment, y_increment, orientation=0):
+        fig, ax = plt.subplots(figsize=(10, 12) if orientation == 0 else (14, 10))
         for row in range(self.height):
             cols = self.width
             if(row%2 == 1):
                 cols = cols - 1
 
             for col in range(cols):
-                x = col * self.hex_size
-                y = row * self.hex_size * 0.9
+                orig_x = col * self.hex_size
+                orig_y = row * self.hex_size * 0.9
                 if(row % 2 == 1):
-                    x += self.hex_size * 0.5
+                    orig_x += self.hex_size * 0.5
+
+                if(orientation == 0):
+                    x = orig_x
+                    y = orig_y
+                else:
+                    x = orig_y
+                    y = -orig_x + (self.width * self.hex_size)
 
                 note = self.getNoteAtIsomorphic(col, row, start_note, x_increment, y_increment)
                 noteName = self.tuning.getIntervalName(note.num)
@@ -53,15 +61,21 @@ class HexPlot():
                 if highlight:
                     colour = highlight.colour
                     ax.annotate(highlight.name, (x, y+0.2), color='black', weight='bold', fontsize=8, ha='center', va='center')
-                hexagon = patches.RegularPolygon((x, y), numVertices=6, radius=self.hex_size*0.55, orientation=0, edgecolor='black', facecolor=colour)
+                hexagon = patches.RegularPolygon((x, y), numVertices=6, radius=self.hex_size*0.55, 
+                                                 orientation=0 if orientation==0 else math.pi*0.5, 
+                                                 edgecolor='black', facecolor=colour)
                 ax.add_patch(hexagon)
                 ax.annotate(f"{noteName}_{note.period}", (x, y), color='black', weight='bold', fontsize=8, ha='center', va='center')
                 ax.annotate(f"{note.num}", (x, y-0.2), color='black', weight='bold', fontsize=8, ha='center', va='center')
 
         ax.set_aspect('equal')
         ax.axis('off')
-        ax.set_xlim(-1, self.width * self.hex_size)
-        ax.set_ylim(-1, self.height * self.hex_size-1)
+        if (orientation == 0):
+            ax.set_xlim(-1, self.width * self.hex_size)
+            ax.set_ylim(-1, self.height * self.hex_size - 1)
+        else:
+            ax.set_xlim(-1, self.height * self.hex_size -1)  # Adjusted for rotation
+            ax.set_ylim(0, self.width * self.hex_size + 1)  # Adjusted for rotation
         plt.show()
 
 
